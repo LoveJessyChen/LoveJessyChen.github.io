93aa60b7a4eebdf59dd301dcd89d2052af5a7ec4
LoveJessyChen <48858531+LoveJessyChen@users.noreply.github.com>
2024-06-02 16:30:51 +0800

    Add files via upload

---
+++ QX-Script/Wink.js
@@ -0,0 +1,23 @@
+
+/******************************
+
+[rewrite_local]
+^https:\/\/api-sub\.meitu\.com url script-response-body https://raw.githubusercontent.com/tangxj123/xian/main/Wink.js
+[mitm] 
+hostname = api-sub.meitu.com
+
+********************************/
+
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/user';
+
+
+if (url.indexOf(vip) != -1) {
+    obj.data.is_vip = true;
+	body = JSON.stringify(obj);
+}
+$done({body});
